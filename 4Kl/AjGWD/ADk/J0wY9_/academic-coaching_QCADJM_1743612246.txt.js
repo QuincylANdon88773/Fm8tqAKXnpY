1ca7b78f6992b3d9a6a8016211636d7c32b2de8f
QuincylANdon88773 <valet.griffon-0c@icloud.com>
2025-05-19 10:51:25 +0200

    Introduce caching mechanism to improve performance in the file

--- 4Kl/AjGWD/ADk/J0wY9_/academic-coaching_QCADJM_1743612246.txt.js
+++ 4Kl/AjGWD/ADk/J0wY9_/academic-coaching_QCADJM_1743612246.txt.js
@@ -32,7 +32,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(mongoSanitize());
-app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
