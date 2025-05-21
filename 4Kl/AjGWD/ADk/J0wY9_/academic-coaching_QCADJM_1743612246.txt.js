eaa2211a9ee848162cf23ee0ea227048fbcdb969
QuincylANdon88773 <valet.griffon-0c@icloud.com>
2025-05-21 19:30:04 +0200

    Add a new API endpoint in the file

--- 4Kl/AjGWD/ADk/J0wY9_/academic-coaching_QCADJM_1743612246.txt.js
+++ 4Kl/AjGWD/ADk/J0wY9_/academic-coaching_QCADJM_1743612246.txt.js
@@ -39,7 +39,6 @@ app.use(cookieParser());
 // const limiter = rateLimit({
 //   windowMs: 15 * 60 * 1000, // 15 minutes
 //   max: 100, // limit each IP to 100 requests per windowMs
-// });
 // app.use(limiter);
 
 const MONGODB_URI = config.MONGODB_URI;
