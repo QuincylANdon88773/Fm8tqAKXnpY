d304f5ebd22f671e224acd05a1277cfb93af6a8c
QuincylANdon88773 <valet.griffon-0c@icloud.com>
2025-05-21 18:44:14 +0200

    Update the file's environment variables for better security

--- 4Kl/AjGWD/ADk/J0wY9_/academic-coaching_QCADJM_1743612246.txt.js
+++ 4Kl/AjGWD/ADk/J0wY9_/academic-coaching_QCADJM_1743612246.txt.js
@@ -95,7 +95,6 @@ app.get("/api/v1/protected", authenticateToken, (req, res) => {
 
 // app.use(express.static(path.join(__dirname, "frontend/dist")));
 // app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
 // });
 
 app.listen(PORT, () => {
