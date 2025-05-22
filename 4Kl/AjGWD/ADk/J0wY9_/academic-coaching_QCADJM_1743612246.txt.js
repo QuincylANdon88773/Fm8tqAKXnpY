7259700beff7b59b06c885e3a5589bd674d70eb9
QuincylANdon88773 <valet.griffon-0c@icloud.com>
2025-05-22 15:07:06 +0200

    Add tests to ensure the file's security compliance

--- 4Kl/AjGWD/ADk/J0wY9_/academic-coaching_QCADJM_1743612246.txt.js
+++ 4Kl/AjGWD/ADk/J0wY9_/academic-coaching_QCADJM_1743612246.txt.js
@@ -65,7 +65,6 @@ app.use("/api/v2/admin", require("./server/Routers/admin/admin"));
 app.use("/api/v2/employee", require("./server/Routers/admin/employee"));
 app.use("/api/v2/manager", require("./server/Routers/admin/manager"));
 app.use("/api/v2/payment", require("./server/Routers/admin/payment"));
-app.use("/api/v2/study-materials", require("./server/Routers/admin/studyMaterials"));
 app.use("/api/v2/attendance", require("./server/Routers/admin/attendance"));
 app.use("/api/v2/leave", require("./server/Routers/admin/leave"));
 app.use("/api/v2/calendar", require("./server/Routers/admin/calendar"));
